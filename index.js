a9025368946a1bdc61e42712d89eee89a7e269f2
Zac Florez <zzac.florez@gmail.com>
2017-07-25 10:56:47 -0600

    change made

--- index.js
+++ index.js
@@ -2,4 +2,8 @@
   'use strict';
 
   // YOUR CODE HERE
+  var controlButton = document.getElementsById('controls')
+  console.log(controlButton[0])
+
+  var controlButton = document.getElementsById
 })();
